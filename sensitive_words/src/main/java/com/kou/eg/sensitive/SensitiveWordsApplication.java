d978f0e1cd63c523b8aca39efcfeb9fa32cd7594
jiajun.kou <jiajun.kou@capgemini.com>
2021-11-22 17:47:55 +0800

    工具类 以及敏感词总结

---
+++ sensitive_words/src/main/java/com/kou/eg/sensitive/SensitiveWordsApplication.java
@@ -0,0 +1,13 @@
+package com.kou.eg.sensitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensitiveWordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensitiveWordsApplication.class, args);
+    }
+
+}
---
+++ sensitive_words/src/test/java/com/kou/eg/sensitive/SensitiveWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kou.eg.sensitive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensitiveWordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
