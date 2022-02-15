230c8bae0149790b9e73a899f89ebbd7985ec9b3
jiajun.kou <jiajun.kou@capgemini.com>
2022-02-15 10:44:03 +0800

    auth

---
+++ base/src/main/java/com/kou/base/Test.java
@@ -0,0 +1,10 @@
+package com.kou.base;
+
+public class Test {
+    public static void main(String[] args) {
+        int i =1;
+        i=i++;
+        System.out.println(i++);
+        System.out.println(i);
+    }
+}
---
+++ nginx/src/test/java/com/kou/nginx/NginxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kou.nginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
