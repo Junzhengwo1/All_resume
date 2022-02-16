24173257b65484a015de0074e6ad7e60111ff956
jiajun.kou <jiajun.kou@capgemini.com>
2022-02-16 17:44:40 +0800

    尚硅谷基础面试方面学习

---
+++ asgg/src/main/java/com/kou/asgg/cas/CasDemo.java
@@ -0,0 +1,14 @@
+package com.kou.asgg.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * CAS demo
+ * 比较交换
+ */
+public class CasDemo {
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+
+    }
+}
