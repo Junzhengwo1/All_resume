7165fa96b9251f74003133a34d8a557ba9590269
jiajun.kou <jiajun.kou@capgemini.com>
2022-09-19 13:03:02 +0800

    auth

---
+++ mymq/src/main/java/com/kou/mymq/MyMqApplication.java
@@ -0,0 +1,13 @@
+package com.kou.mymq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMqApplication.class, args);
+    }
+
+}
---
+++ mymq/src/test/java/com/kou/mymq/MymqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kou.mymq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
