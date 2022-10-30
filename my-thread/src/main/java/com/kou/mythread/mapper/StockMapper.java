f3ddd54374fb52589cadc2e890700b717ad68f65
koujiajun <2627084538@qq.com>
2022-10-30 16:02:30 +0800

    a

--- my-thread/src/main/java/com/kou/mythread/mapper/StockMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kou.mythread.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kou.mythread.pojo.StockPojo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface StockMapper extends BaseMapper<StockPojo> {
-}
